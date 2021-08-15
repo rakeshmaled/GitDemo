08e6b15983039cad0b9d1f2da775cbfa539845b8
rakesh <rakesh.r.maled@gmail.com>
2021-08-15 14:31:45 +0530

    Final commit by American

---
+++ src/test/java/qaclickacademy/demo.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is rakesh r maled");
+
+	}
+
+}
