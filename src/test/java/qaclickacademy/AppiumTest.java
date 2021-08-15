98780a5226fd10afc61a737b453a2485e045580e
rakesh <rakesh.r.maled@gmail.com>
2021-08-15 12:04:40 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAPPandroid()
+	{
+		System.out.println("NativeAPPandroid");
+	}
+	
+	
+	@Test
+	public void IOSapp()
+	{
+		System.out.println("IOSapp");
+	}
+	
+	
+	
+	
+	
+}
